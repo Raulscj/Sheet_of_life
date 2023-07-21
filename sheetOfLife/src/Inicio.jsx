73193ef88632808466faaa6112c695b2ac20fd71
Raulscj <92169825+Raulscj@users.noreply.github.com>
2023-07-20 23:17:41 -0400

    Primeras Tablas

--- sheetOfLife/src/Inicio.jsx
+++ sheetOfLife/src/Inicio.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Inicio() {
   return (
     <>
       <div>Inicio</div>
+      <Link to={"/Raul"}>
+        <button type="button" class="btn btn-outline-primary">
+          Raul
+        </button>
+      </Link>
+      <img src="./multimedia/Banner.jpg" alt="" />
     </>
   );
 }
--- sheetOfLife/src/Raul.jsx
+++ sheetOfLife/src/Raul.jsx
@@ -2,71 +2,81 @@ import React from "react";
 
 export default function Raul() {
   return (
-    <>
-      {/* Imagen de perfil */}
-      <p>
-        Plata II Edif. Jose Felix Rivas PB Apart 01 VALERA EDO. TRUJILLO -
-        VENEZUELA. TELÉFONO 0271-2254842
-      </p>
-      <div className="container" id="Datos">
-        <img />
-        <h2>Raúl González</h2>
-        <hr />
-        <h3>Datos Personales</h3>
-        <div>Cedula de Identidad</div>
-        <p>26.962.904</p>
-        <div>Telefono</div>
-        <p>0416-1187577</p>
-        <div>Apellidos</div>
-        <p>González González</p>
-        <div>Nombres</div>
-        <p>Raúl Alfonso del Sagrado Corazón de Jesús</p>
-        <div>Sexo</div>
-        <p>Masculino</p>
-        <div>Estado civil</div>
-        <p>Soltero</p>
-        <div>País de Nacimiento</div>
-        <p>Venezuela</p>
-        <div>Estado de nacimiento</div>
-        <p>Mérida</p>
-        <div>Correo</div>
-        <p>radscjggdiosteama@gmail.com</p>
-        <div>Fecha de nacimiento</div>
-        <p>25/04/199</p>
-      </div>
-
-      <div className="container" id="Estudios">
-        <h4>Estudios Realizados</h4>
-        <div>Primaria</div>
-        <p>U.E Hermanas María Sorrosal 1er Grado a 6to Grado </p>
-        <div>Secundaria</div>
-        <p>
-          Ciclo Diversificado “C.P. Monseñor Mejia”. Valera Edo. Trujillo
-          Venezuela Título obtenido: Bachiller en Ciencias
-        </p>
-        <div>Universitario</div>
-        <p>
-          Universidad Politécnica Territorial del Estado Trujillo Mario Briceño
-          Iragorry Titulo obtenido:TSU en Informática
-        </p>
-      </div>
-      <div className="container" id="Cursos">
-        <h4>Cursos y talleres realizados</h4>
-        <h5>Nombre del Curso</h5>
-        <h5>Institución</h5>
-        <h5>Horas</h5>
-        <h5>Fecha</h5>
-      </div>
-      <div className="container" id="Habilidades">
-        <h4>Conocimientos, habilidades y destrezas</h4>
-        <p>
-          Amplios Conocimientos en Programas de Computación, Manejo Óptimo de
-          Internet, Manejo Óptimo de Office. Manejo de Personal, Facilidad de
-          Aprendizaje y de Establecer Buenas Relaciones con Grupos de Trabajo.
-          Dispuesto a Trabajar en Ambientes Bajo Presión. Capacidad de trabajar
-          en Grupo. Animar con Valores a Niños, adolescentes, jóvenes y adultos.
-        </p>
-      </div>
-    </>
+    <div className="container mt-3">
+      <h2>Raúl González</h2>
+      <hr />
+      <table className="table table-dark table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th colspan="3" justify="center">
+              DATOS PERSONALES
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Cedula de Identidad</th>
+            <td>26.962.904</td>
+            <th>Teléfono</th>
+            <td>0416-1187577</td>
+          </tr>
+          <tr>
+            <th>Apellidos</th>
+            <td>González González</td>
+            <th>Nombres</th>
+            <td>Raúl Alfonso del Sagrado Corazón de Jesús</td>
+          </tr>
+          <tr>
+            <th>Sexo</th>
+            <td>Masculino</td>
+            <th>Estado civil</th>
+            <td>Soltero</td>
+          </tr>
+          <tr>
+            <th>País de Nacimiento</th>
+            <td>Venezuela</td>
+            <th>Estado de nacimiento</th>
+            <td>Mérida</td>
+          </tr>
+          <tr>
+            <th>Correo</th>
+            <td>radscjggdiosteama@gmail.com</td>
+            <th>Fecha de nacimiento</th>
+            <td>25/04/1999</td>
+          </tr>
+        </tbody>
+      </table>
+      <hr />
+      <table className="table table-hover">
+        <thead>
+          <tr className="table-primary">
+            <th colspan="4">ESTUDIOS REALIZADOS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="table-info">
+            <th colspan="1">Primaria</th>
+            <td colspan="3">
+              U.E Hermanas María Sorrosal 1er Grado a 6to Grado
+            </td>
+          </tr>
+          <tr className="table-primary">
+            <th colspan="1">Secundaria</th>
+            <td colspan="3">
+              Ciclo Diversificado “C.P. Monseñor Mejia”. Valera Edo. Trujillo
+              Venezuela Título obtenido: Bachiller en Ciencias
+            </td>
+          </tr>
+          <tr className="table-info">
+            <th colspan="1">Universitario</th>
+            <td colspan="3">
+              Universidad Politécnica Territorial del Estado Trujillo "Mario
+              Briceño Iragorry" Titulo obtenido:TSU en Informática
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
